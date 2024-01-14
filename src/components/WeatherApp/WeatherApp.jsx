8ea5d5462ba0f08286c8b46f69fd803e54f46676
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-14 13:37:22 +0100

    creada estructura card tiempo local

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,14 +1,20 @@
-import { useEffect } from "react";
 import "./WeatherApp.css";
+import "./animatedIcons.css"
+import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import  Icon from "../../assets/amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg?react";
+
 
 const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?";
 const API_KEY = "c5a48b3e59d242aedae7b2fb0b9ad0e4";
 
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
 export default function WeatherApp() {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+  const [data, setData] = useState({});
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -24,14 +30,25 @@ export default function WeatherApp() {
           `${API_ENDPOINT}lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=es`
         )
         .then((response) => {
-          console.log(response.data);
+          setData(response.data);
         });
     }
   }, [latitude, longitude]);
 
   return (
-    <div>
-      <h1>WeatherApp</h1>
-    </div>
+    <main>
+      {data.main && (
+        <article className="current-weather-card">
+          <h1>{data.name}</h1>
+          <h3>{data.main.temp.toFixed(1)}°</h3>
+          <h4>{data.weather[0].description}</h4>
+          <span className="temp-max-min">
+            <h5>Máx: {data.main.temp_max.toFixed(1)}</h5>
+            <h5>Mín: {data.main.temp_min.toFixed(1)}</h5>
+          </span>
+          <Icon />
+        </article>
+      )}
+    </main>
   );
 }
