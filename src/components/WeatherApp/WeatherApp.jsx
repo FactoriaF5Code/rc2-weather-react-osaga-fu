3a5790bb967e2d4a42fbd60f79bb05982f009059
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-14 20:00:30 +0100

    eliminado componente search

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -2,7 +2,7 @@ import "./WeatherApp.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
-import SearchTool from "../SearchTool/SearchTool";
+
 
 const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?";
 const API_KEY = "c5a48b3e59d242aedae7b2fb0b9ad0e4";
