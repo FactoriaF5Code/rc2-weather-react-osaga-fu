06059713517cc7f21aa070043e15be3affc9dbd1
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-14 19:45:38 +0100

    añadida barra de busqueda

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -2,15 +2,15 @@ import "./WeatherApp.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import SearchTool from "../SearchTool/SearchTool";
 
 const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?";
 const API_KEY = "c5a48b3e59d242aedae7b2fb0b9ad0e4";
 const ICON_ENDPOINT =
   "https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/";
 
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
 export default function WeatherApp() {
+
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [data, setData] = useState({});
@@ -18,20 +18,20 @@ export default function WeatherApp() {
   const iconPaths = {
     "01d": ICON_ENDPOINT + "sunny.svg",
     "01n": ICON_ENDPOINT + "night.svg",
-    "02d" : ICON_ENDPOINT + "cloudy-day-1.svg",
-    "02n" : ICON_ENDPOINT + "cloudy-night-1.svg",
-    "03d" : ICON_ENDPOINT + "cloudy-day-3.svg",
-    "03n" : ICON_ENDPOINT + "cloudy-night-3.svg",
-    "04d" : ICON_ENDPOINT + "cloudy.svg",
-    "04n" : ICON_ENDPOINT + "cloudy.svg",
-    "09d" : ICON_ENDPOINT + "rainy-6.svg",
-    "09n" : ICON_ENDPOINT + "rainy-6.svg",
-    "10d" : ICON_ENDPOINT + "rainy-4.svg",
-    "10n" : ICON_ENDPOINT + "rainy-4.svg",
-    "11d" : ICON_ENDPOINT + "thunder.svg",
-    "11n" : ICON_ENDPOINT + "thunder.svg",
-    "13d" : ICON_ENDPOINT + "snowy-6.svg",
-    "13n" : ICON_ENDPOINT + "snowy-6.svg",
+    "02d": ICON_ENDPOINT + "cloudy-day-1.svg",
+    "02n": ICON_ENDPOINT + "cloudy-night-1.svg",
+    "03d": ICON_ENDPOINT + "cloudy-day-3.svg",
+    "03n": ICON_ENDPOINT + "cloudy-night-3.svg",
+    "04d": ICON_ENDPOINT + "cloudy.svg",
+    "04n": ICON_ENDPOINT + "cloudy.svg",
+    "09d": ICON_ENDPOINT + "rainy-6.svg",
+    "09n": ICON_ENDPOINT + "rainy-6.svg",
+    "10d": ICON_ENDPOINT + "rainy-4.svg",
+    "10n": ICON_ENDPOINT + "rainy-4.svg",
+    "11d": ICON_ENDPOINT + "thunder.svg",
+    "11n": ICON_ENDPOINT + "thunder.svg",
+    "13d": ICON_ENDPOINT + "snowy-6.svg",
+    "13n": ICON_ENDPOINT + "snowy-6.svg",
   };
 
   useEffect(() => {
@@ -57,7 +57,6 @@ export default function WeatherApp() {
     <main>
       {data.main && (
         <article className="current-weather-card">
-        
           <h3 className="location-name">{data.name}</h3>
           <span>
             <h1 className="location-temp">{data.main.temp.toFixed(1)}</h1>
@@ -75,7 +74,7 @@ export default function WeatherApp() {
             src={iconPaths[data.weather[0].icon]}
             alt=""
           />
-    
+          <input className="search-input" type="text" placeholder="Buscar por ciudad"/>
         </article>
       )}
     </main>
