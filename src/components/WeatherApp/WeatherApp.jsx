41db4b7b30adc659d2672c348eab50e24ddb4605
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-14 10:53:08 +0100

    accediendo a la api con geolocalización

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -1,17 +1,14 @@
 import { useEffect } from "react";
 import "./WeatherApp.css";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-const API_ENDPOINT = "https://api.openweathermap.org/data/3.0/onecall?";
+const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?";
 const API_KEY = "c5a48b3e59d242aedae7b2fb0b9ad0e4";
 
-
-
 export default function WeatherApp() {
-
-    const [latitude, setLatitude] = useState("");
-    const [longitude, setLongitude] = useState("")
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -21,12 +18,16 @@ export default function WeatherApp() {
 
     console.log(latitude, longitude);
 
-    // axios.get( `${API_ENDPOINT}lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
-    // .then((response) => {
-    //     console.log(response.data)
-    // });
-
-  }, []);
+    if (latitude !== "" && longitude !== "") {
+      axios
+        .get(
+          `${API_ENDPOINT}lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=es`
+        )
+        .then((response) => {
+          console.log(response.data);
+        });
+    }
+  }, [latitude, longitude]);
 
   return (
     <div>
