54a70c2ac57e3c04968340e0f6fc7ae80348f889
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-01-23 19:44:13 +0100

    añadida prediccion por horas a falta de refactorizar

---
+++ src/components/ForecastHourly/ForecastHourly.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import "./ForecastHourly.css";
+import axios from "axios";
+
+const API_FORECAST = `http://api.openweathermap.org/data/2.5/forecast?`;
+const API_CITY = `q={city name}&appid={API key}&cnt=5&lang=es&units=metric`;
+
+const API_KEY = "c5a48b3e59d242aedae7b2fb0b9ad0e4";
+const ICON_ENDPOINT =
+  "https://www.amcharts.com/wp-content/themes/amcharts4/css/img/icons/weather/animated/";
+
+const iconPaths = {
+    "01d": ICON_ENDPOINT + "day.svg",
+    "01n": ICON_ENDPOINT + "night.svg",
+    "02d": ICON_ENDPOINT + "cloudy-day-1.svg",
+    "02n": ICON_ENDPOINT + "cloudy-night-1.svg",
+    "03d": ICON_ENDPOINT + "cloudy-day-3.svg",
+    "03n": ICON_ENDPOINT + "cloudy-night-3.svg",
+    "04d": ICON_ENDPOINT + "cloudy.svg",
+    "04n": ICON_ENDPOINT + "cloudy.svg",
+    "09d": ICON_ENDPOINT + "rainy-6.svg",
+    "09n": ICON_ENDPOINT + "rainy-6.svg",
+    "10d": ICON_ENDPOINT + "rainy-4.svg",
+    "10n": ICON_ENDPOINT + "rainy-4.svg",
+    "11d": ICON_ENDPOINT + "thunder.svg",
+    "11n": ICON_ENDPOINT + "thunder.svg",
+    "13d": ICON_ENDPOINT + "snowy-6.svg",
+    "13n": ICON_ENDPOINT + "snowy-6.svg",
+  };
+
+  
+export default function ForecastHourly({ latitude, longitude }) {
+  const [forecastHourly, setForecastHourly] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (latitude !== "" && longitude !== "") {
+          const response = await axios.get(
+            `${API_FORECAST}lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=es&cnt=5`
+          );
+          setForecastHourly(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [latitude, longitude]);
+
+  return (
+    <section className="hourly-forecast">
+      <h1 className="hourly-forecast-title">PREDICCIÓN POR HORAS</h1>
+      <div className="hourly-cards-container">
+        {forecastHourly.list && (
+            forecastHourly.list.map((hora)=>( <article className="hourly-forecast-card" key={hora.dt}>
+            <h4 className="forecast-card-time">
+              {new Date(hora.dt * 1000).getHours()-1}h
+            </h4>
+            <img className="forecast-weather-icon" src={iconPaths[hora.weather[0].icon]} alt="no hay icono" />
+            <h3 className="forecast-card-temp">{hora.main.temp.toFixed(1)}°</h3>
+          </article>
+         )  )
+        //   <article className="hourly-forecast-card">
+        //     <h4 className="forecast-card-time">
+        //       {new Date(forecastHourly.list[0].dt * 1000).getHours()-1}h
+        //     </h4>
+        //     <img className="forecast-weather-icon" src={iconPaths[forecastHourly.list[0].weather[0].icon]} alt="no hay icono" />
+        //     <h3 className="forecast-card-temp">{forecastHourly.list[0].main.temp.toFixed(1)}°</h3>
+        //   </article>
+        )}
+      </div>
+    </section>
+  );
+}
--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -2,6 +2,7 @@ import "./WeatherApp.css";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import ForecastHourly from "../ForecastHourly/ForecastHourly";
 
 const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather?";
 const API_KEY = "c5a48b3e59d242aedae7b2fb0b9ad0e4";
@@ -64,33 +65,37 @@ export default function WeatherApp() {
   return (
     <main>
       {data.main && (
-        <article className="current-weather-card">
-          <h3 className="location-name">{data.name}</h3>
-          <span>
-            <h1 className="location-temp">{data.main.temp.toFixed(1)}</h1>
-            <p className="degree-symbol">°</p>
-          </span>
-          <h4 className="location-weather-state">
-            {data.weather[0].description}
-          </h4>
-          <span className="temp-max-min">
-            <h5>Máx: {data.main.temp_max.toFixed(1)}°</h5>
-            <h5>Mín: {data.main.temp_min.toFixed(1)}°</h5>
-          </span>
-          <img
-            className="animated-weather-icon"
-            src={iconPaths[data.weather[0].icon]}
-            alt=""
-          />
-          <input
-            className="search-input"
-            type="text"
-            placeholder="Buscar por ciudad"
-            value={location}
-            onChange={(event) => setLocation(event.target.value)}
-            onKeyDown={searchLocation}
-          />
-        </article>
+        <div className="cards-container">
+          <article className="current-weather-card">
+            <h3 className="location-name">{data.name}</h3>
+            <span>
+              <h1 className="location-temp">{data.main.temp.toFixed(1)}</h1>
+              <p className="degree-symbol">°</p>
+            </span>
+            <h4 className="location-weather-state">
+              {data.weather[0].description}
+            </h4>
+            <span className="temp-max-min">
+              <h5>Máx: {data.main.temp_max.toFixed(1)}°</h5>
+              <h5>Mín: {data.main.temp_min.toFixed(1)}°</h5>
+            </span>
+            <img
+              className="animated-weather-icon"
+              src={iconPaths[data.weather[0].icon]}
+              alt=""
+            />
+            <input
+              className="search-input"
+              type="text"
+              placeholder="Buscar por ciudad"
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
+              onKeyDown={searchLocation}
+            />
+          </article>
+          <ForecastHourly latitude={latitude} longitude={longitude}/>
+          <section className="daily-forecast-card"></section>
+        </div>
       )}
     </main>
   );
